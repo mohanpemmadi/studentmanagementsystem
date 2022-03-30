e6a60010f11e35173418587ce0b2f09d9f91f4e2
mohanpemmadi <connectpypi@gmail.com>
2022-03-30 07:52:09 +0530

    fix

--- student/models.py
+++ student/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class StudentTable(models.Model):
+    name = models.CharField(max_length=100)
+    grade = models.IntegerField()
+    city = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
+    def __str__(self):
+        return self.name
+
+
+
